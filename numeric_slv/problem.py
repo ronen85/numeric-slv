b8f265ce799b786defe8ad0b56151871ece48552
ronen85 <ronen.nx@gmail.com>
2022-02-13 15:37:37 +0200

    add remove_constants

--- numeric_slv/problem.py
+++ numeric_slv/problem.py
@@ -11,30 +11,13 @@ from translate.pddl import Action, Atom, FunctionComparison, Effect, NegatedAtom
     Conjunction, PrimitiveNumericExpression, NumericConstant, Task, FunctionAssignment, NumericEffect, Literal, \
     ArithmeticExpression, Sum, Product, Predicate, Function, conditions, TypedObject, Assign
 from .ground_task import get_grounded_task
+from .remove_constants import get_constants_from_grounded_task, replace_pne_with_numeric_constants, \
+    get_task_with_grounded_constants
 
 from .task_to_pddl import get_pddl_domain, get_pddl_prob, as_pddl
 
 
-def get_constants_from_grounded_task(grounded_task):
-    """
-    gets a grounded task (with grounded actions (no parameters)) and returns a dictionary with constants
-    (functions, i.e. numerical variables, that the agents' actions do not change)
-    """
-    assign_dict = dict([(assign.fluent, assign.expression) for assign in grounded_task.num_init])
-    variables_from_init = set([a.fluent for a in grounded_task.num_init])
-    variables_from_action_effects = set()
-    for action in grounded_task.actions:
-        for eff in action.effects:
-            if isinstance(eff.peffect, Literal):
-                continue
-            elif isinstance(eff.peffect, FunctionAssignment):
-                assert isinstance(eff.peffect.fluent, PrimitiveNumericExpression), 'expected PNE object'
-                variables_from_action_effects.add(eff.peffect.fluent)
-            else:
-                raise NotImplemented
-    constants = variables_from_init - variables_from_action_effects
-    constants_dict = deepcopy(dict([(pne, assign_dict[pne]) for pne in constants]))
-    return constants_dict
+
 
 
 def get_numerical_preconditions_from_action(action: Action):
@@ -63,54 +46,7 @@ def replace_punctuation(s, replace=' '):
     return re.sub(r'[' + chars + ']', replace, s)
 
 
-def replace_pne_with_numeric_constants(obj, constants_dict):
-    """
-    gets an object (e.g. Action, Conjunction) and a dictionary with constants information
-    and returns the same object with constants (instead of numeric expressions)
-    """
-    if isinstance(obj, Task):
-        modified_task = deepcopy(obj)
-        modified_task.actions = [replace_pne_with_numeric_constants(a, constants_dict) for a in modified_task.actions]
-        return modified_task
-    elif isinstance(obj, Action):
-        modified_action = deepcopy(obj)
-        modified_action.precondition = replace_pne_with_numeric_constants(modified_action.precondition, constants_dict)
-        modified_action.effects = [replace_pne_with_numeric_constants(eff, constants_dict) for eff in
-                                   modified_action.effects]
-        return modified_action
-    elif isinstance(obj, Conjunction):
-        modified_conjunction = deepcopy(obj)
-        modified_conjunction.parts = [replace_pne_with_numeric_constants(p, constants_dict) for p in
-                                      modified_conjunction.parts]
-        return modified_conjunction
-    elif isinstance(obj, Literal):
-        return deepcopy(obj)
-    elif isinstance(obj, FunctionComparison):
-        modified_comparison = deepcopy(obj)
-        modified_comparison.parts = [replace_pne_with_numeric_constants(p, constants_dict) for p in
-                                     modified_comparison.parts]
-        return modified_comparison
-    elif isinstance(obj, ArithmeticExpression):
-        modified_expression = deepcopy(obj)
-        modified_expression.parts = [replace_pne_with_numeric_constants(p, constants_dict) for p in
-                                     modified_expression.parts]
-        return modified_expression
-    elif isinstance(obj, Effect):
-        modified_eff = deepcopy(obj)
-        modified_eff.peffect = replace_pne_with_numeric_constants(modified_eff.peffect, constants_dict)
-        return modified_eff
-    elif isinstance(obj, FunctionAssignment):
-        f_assignment = deepcopy(obj)
-        assert isinstance(f_assignment.fluent,
-                          PrimitiveNumericExpression), f'expected a PNE, got: {f_assignment.fluent}'
-        f_assignment.expression = replace_pne_with_numeric_constants(f_assignment.expression, constants_dict)
-        return f_assignment
-    elif isinstance(obj, PrimitiveNumericExpression):
-        return constants_dict[obj] if obj in constants_dict.keys() else deepcopy(obj)
-    elif isinstance(obj, NumericConstant):
-        return deepcopy(obj)
-    else:
-        raise NotImplemented
+
 
 
 def arithmetic_expression_to_sym(exp, pne_dict=dict()):
@@ -447,14 +383,13 @@ def replace_complex_numerical_expressions_with_zeta_variables(obj, zeta_dict_lis
         raise NotImplemented
 
 
+
 def get_simplified_grounded_task(domain_filename, prob_filename):
     grounded_task = get_grounded_task(domain_filename, prob_filename)
-    # get constants
-    constants_dict = get_constants_from_grounded_task(grounded_task)
-    # modify task - replace numeric expressions with numeric constants
-    modified_grounded_task = replace_pne_with_numeric_constants(grounded_task, constants_dict)
+    task_with_grounded_constants = get_task_with_grounded_constants(grounded_task)
+
     # modify task - convert numerical expressions to normal form
-    modified_grounded_task = convert_numerical_expressions_to_normal_form(modified_grounded_task)
+    modified_grounded_task = convert_numerical_expressions_to_normal_form(task_with_grounded_constants)
     # find and replaces complex numerical expressions and create new zeta variables
     domain = get_pddl_domain(modified_grounded_task)
     modified_grounded_task = replace_complex_numerical_expressions_with_zeta_variables(modified_grounded_task)
---
+++ numeric_slv/remove_constants.py
@@ -0,0 +1,83 @@
+from copy import deepcopy
+
+from translate.pddl import FunctionAssignment, Literal, PrimitiveNumericExpression, Task, Action, Conjunction, \
+    FunctionComparison, ArithmeticExpression, Effect, NumericConstant
+
+
+def get_constants_from_grounded_task(grounded_task):
+    """
+    gets a grounded task (with grounded actions (no parameters)) and returns a dictionary with constants
+    (functions, i.e. numerical variables, that the agents' actions do not change)
+    """
+    assign_dict = dict([(assign.fluent, assign.expression) for assign in grounded_task.num_init])
+    variables_from_init = set([a.fluent for a in grounded_task.num_init])
+    variables_from_action_effects = set()
+    for action in grounded_task.actions:
+        for eff in action.effects:
+            if isinstance(eff.peffect, Literal):
+                continue
+            elif isinstance(eff.peffect, FunctionAssignment):
+                assert isinstance(eff.peffect.fluent, PrimitiveNumericExpression), 'expected PNE object'
+                variables_from_action_effects.add(eff.peffect.fluent)
+            else:
+                raise NotImplemented
+    constants = variables_from_init - variables_from_action_effects
+    constants_dict = deepcopy(dict([(pne, assign_dict[pne]) for pne in constants]))
+    return constants_dict
+
+def replace_pne_with_numeric_constants(obj, constants_dict):
+    """
+    gets an object (e.g. Action, Conjunction) and a dictionary with constants information
+    and returns the same object with constants (instead of numeric expressions)
+    """
+    if isinstance(obj, Task):
+        modified_task = deepcopy(obj)
+        modified_task.actions = [replace_pne_with_numeric_constants(a, constants_dict) for a in modified_task.actions]
+        return modified_task
+    elif isinstance(obj, Action):
+        modified_action = deepcopy(obj)
+        modified_action.precondition = replace_pne_with_numeric_constants(modified_action.precondition, constants_dict)
+        modified_action.effects = [replace_pne_with_numeric_constants(eff, constants_dict) for eff in
+                                   modified_action.effects]
+        return modified_action
+    elif isinstance(obj, Conjunction):
+        modified_conjunction = deepcopy(obj)
+        modified_conjunction.parts = [replace_pne_with_numeric_constants(p, constants_dict) for p in
+                                      modified_conjunction.parts]
+        return modified_conjunction
+    elif isinstance(obj, Literal):
+        return deepcopy(obj)
+    elif isinstance(obj, FunctionComparison):
+        modified_comparison = deepcopy(obj)
+        modified_comparison.parts = [replace_pne_with_numeric_constants(p, constants_dict) for p in
+                                     modified_comparison.parts]
+        return modified_comparison
+    elif isinstance(obj, ArithmeticExpression):
+        modified_expression = deepcopy(obj)
+        modified_expression.parts = [replace_pne_with_numeric_constants(p, constants_dict) for p in
+                                     modified_expression.parts]
+        return modified_expression
+    elif isinstance(obj, Effect):
+        modified_eff = deepcopy(obj)
+        modified_eff.peffect = replace_pne_with_numeric_constants(modified_eff.peffect, constants_dict)
+        return modified_eff
+    elif isinstance(obj, FunctionAssignment):
+        f_assignment = deepcopy(obj)
+        assert isinstance(f_assignment.fluent,
+                          PrimitiveNumericExpression), f'expected a PNE, got: {f_assignment.fluent}'
+        f_assignment.expression = replace_pne_with_numeric_constants(f_assignment.expression, constants_dict)
+        return f_assignment
+    elif isinstance(obj, PrimitiveNumericExpression):
+        return constants_dict[obj] if obj in constants_dict.keys() else deepcopy(obj)
+    elif isinstance(obj, NumericConstant):
+        return deepcopy(obj)
+    else:
+        raise NotImplemented
+
+
+def get_task_with_grounded_constants(grounded_task):
+    # get constants
+    constants_dict = get_constants_from_grounded_task(grounded_task)
+    # modify task - replace numeric expressions with numeric constants
+    modified_grounded_task = replace_pne_with_numeric_constants(grounded_task, constants_dict)
+    return modified_grounded_task
