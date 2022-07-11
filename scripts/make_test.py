466a0caf7ca07923a562d1fd0e1ed791c9b48972
ronen85 <ronen.nx@gmail.com>
2022-07-11 10:17:39 +0300

    add make_test script

---
+++ scripts/make_test.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+arg1_list = ['depots']
+arg2_list = list(range(1,21))
+arguments = list(product(arg1_list, arg2_list))
+cmd_list = [f'python run.py {a} {b}\n' for a,b in arguments]
+test_file = 'test.sh'
+with open(test_file, 'w') as f:
+    f.writelines(cmd_list)
